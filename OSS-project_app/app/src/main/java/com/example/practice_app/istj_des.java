8ad2b4501c84d946ec381d728180b0de5282a1a4
조용찬 <101387953+dyongchan@users.noreply.github.com>
2022-05-25 22:57:39 +0900

    20220525-3

---
+++ OSS-project_app/app/src/main/java/com/example/practice_app/istj_des.java
@@ -0,0 +1,14 @@
+package com.example.practice_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class istj_des extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.istj_des);
+    }
+}
